7ad656abbf966eeb685d68ea3cf0b9b1a9a17dcf
hy125245 <hy125245@163.com>
2024-07-12 23:38:15 +0800

    2027-7-12

---
+++ 100.相同的树.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=cpp
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    bool compare_tree(TreeNode* p, TreeNode* q)
+    {
+        while(nullptr == p && nullptr == q)
+        {
+            return true;
+        }
+        compare_tree()
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+    }
+};
+// @lc code=end
+
