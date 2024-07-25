0c3b2b278b8d530b56cf0685f2d8ce2ef1b7f2ef
hy125245 <hy125245@163.com>
2024-07-25 23:54:20 +0800

    2024-7-25

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -4,16 +4,23 @@
  * [3] 无重复字符的最长子串
  */
 #include <iostream>
-#include <String>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 // @lc code=start
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        for(int i = 0; i < s.size(); i++)
-        {
-            
+        vector<int> m(128, 0);
+        int ans = 0;
+        int i = 0;
+        for (int j = 0; j < s.size(); j++) {
+            i = max(i, m[s[j]]);
+            m[s[j]] = j + 1;
+            ans = max(ans, j - i + 1);
         }
+        return ans;
     }
 };
 // @lc code=end
